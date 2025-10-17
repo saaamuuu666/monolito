47295c3edb2b1f7130c3a7df972442de13d290d6
DESKTOP-BO99ID7\Samu <samuelsolmos@gmail.com>
2025-10-17 15:31:26 +0200

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,26 +3,26 @@ import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import pluginPrettier from 'eslint-plugin-prettier';
 export default defineConfig([
-{
-files: ['**/*.{js,mjs,cjs}'],
-plugins: { js },
-extends: ['js/recommended'],
-languageOptions: { globals: globals.browser },
-},
-{
-files: ['**/*.js'],
-languageOptions: {
-sourceType: 'commonjs',
-globals: {
-...globals.node,
-...globals.es2021,
-},
-},
-plugins: { prettier: pluginPrettier },
-rules: {
-semi: ['error', 'always'],
-quotes: ['error', 'single'],
-'prettier/prettier': 'error',
-},
-},
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+    },
+  },
 ]);
